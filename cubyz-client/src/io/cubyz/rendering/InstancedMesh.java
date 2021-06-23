5a4dfea052568a7d778408ca74d5140b8687366a
MariaChatzii <maria2312ch@gmail.com>
2021-06-24 02:43:51 +0300

    refactoring again InstancedMesh

--- cubyz-client/src/io/cubyz/rendering/InstancedMesh.java
+++ cubyz-client/src/io/cubyz/rendering/InstancedMesh.java
@@ -40,90 +40,35 @@ public class InstancedMesh extends Mesh {
 	public boolean isInstanced() {
 		return true;
 	}
-	
+
 	public InstancedMesh(int vao, int count, List<Integer> vaoIds, Model model, int numInstances) {
 		super(vao, count, vaoIds, model);
 		glBindVertexArray(vaoId);
 		modelViewVBO = glGenBuffers();
 		initInstances(numInstances);
 	}
-	
+
 	protected void initInstances(int numInstances) {
 		this.numInstances = numInstances;
 		vboIdList.add(modelViewVBO);
 		instanceDataBuffer = MemoryUtil.memAllocFloat(numInstances * INSTANCE_SIZE_FLOATS);
-		glBindBuffer(GL_ARRAY_BUFFER, modelViewVBO);
-		int start = 3;
-		int strideStart = 0;
-		// Model matrix:
-		for (int i = 0; i < 4; i++) {
-			glVertexAttribPointer(start, 4, GL_FLOAT, false, INSTANCE_SIZE_BYTES, strideStart);
-			glVertexAttribDivisor(start, 1);
-			start++;
-			strideStart += VECTOR4F_SIZE_BYTES;
-		}
-		// Light Color:
-		for(int i = 0; i < 8; i++) {
-			glVertexAttribPointer(start, 1, GL_FLOAT, false, INSTANCE_SIZE_BYTES, strideStart);
-			glVertexAttribDivisor(start, 1);
-			start++;
-			strideStart += FLOAT_SIZE_BYTES;
-		}
-		// Selection/Breaking: 0 = not selected
-		glVertexAttribPointer(start, 1, GL_FLOAT, false, INSTANCE_SIZE_BYTES, strideStart);
-		glVertexAttribDivisor(start, 1);
-		start++;
-		strideStart += FLOAT_SIZE_BYTES;
 
-		// Light view matrix
-		
-		for (int i = 0; i < 4; i++) {
-			glVertexAttribPointer(start, 4, GL_FLOAT, false, INSTANCE_SIZE_BYTES, strideStart);
-			glVertexAttribDivisor(start, 1);
-			start++;
-			strideStart += VECTOR4F_SIZE_BYTES;
-		}
-
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindVertexArray(0);
+		setInstancesInfo(4, 8, true);
 	}
-	
+
 	public InstancedMesh(Model model, int numInstances) {
 		super(model);
 		glBindVertexArray(vaoId);
 		modelViewVBO = glGenBuffers();
 		initInstances(numInstances);
 	}
-	
+
 	public void setInstances(int numInst) {
 		this.numInstances = numInst;
 		glBindVertexArray(vaoId);
 		instanceDataBuffer = MemoryUtil.memRealloc(instanceDataBuffer, numInstances * INSTANCE_SIZE_FLOATS);
-		glBindBuffer(GL_ARRAY_BUFFER, modelViewVBO);
-		int start = 3;
-		int strideStart = 0;
-		// Model matrix:
-		for (int i = 0; i < 3; i++) {
-			glVertexAttribPointer(start, 4, GL_FLOAT, false, INSTANCE_SIZE_BYTES, strideStart);
-			glVertexAttribDivisor(start, 1);
-			start++;
-			strideStart += VECTOR4F_SIZE_BYTES;
-		}
-		// Light Color:
-		for(int i = 0; i < 8; i++) {
-			glVertexAttribPointer(start, 1, GL_FLOAT, false, INSTANCE_SIZE_BYTES, strideStart);
-			glVertexAttribDivisor(start, 1);
-			start++;
-			strideStart += FLOAT_SIZE_BYTES;
-		}
-		// Selection:
-		glVertexAttribPointer(start, 1, GL_FLOAT, false, INSTANCE_SIZE_BYTES, strideStart);
-		glVertexAttribDivisor(start, 1);
-		start++;
-		strideStart += FLOAT_SIZE_BYTES;
 
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindVertexArray(0);
+		setInstancesInfo(3, 8, false);
 	}
 
 	public Mesh cloneNoMaterial() {
@@ -132,7 +77,7 @@ public class InstancedMesh extends Mesh {
 		clone.frustum = frustum;
 		return clone;
 	}
-	
+
 	@Override
 	public void cleanUp() {
 		super.cleanUp();
@@ -165,8 +110,8 @@ public class InstancedMesh extends Mesh {
 	}
 
 	int oldSize = 0;
-	
-	/**
+
+	/*
 	 * Render list instanced in a non-chunked way
 	 * @param spatials
 	 * @param transformation
@@ -174,6 +119,7 @@ public class InstancedMesh extends Mesh {
 	 * @param useTextureAtlas
 	 */
 	@Deprecated
+
 	public boolean renderListInstancedNC(FastList<Spatial> spatials, Transformation transformation, boolean useTextureAtlas) {
 		if (numInstances == 0)
 			return false;
@@ -189,7 +135,7 @@ public class InstancedMesh extends Mesh {
 			bool = true;
 		}
 		renderChunkInstanced(spatials.size, transformation);
-		
+
 		endRender();
 		return bool;
 	}
@@ -210,11 +156,11 @@ public class InstancedMesh extends Mesh {
 
 		endRender();
 	}
-	
+
 	@Deprecated
 	public void uploadData(Spatial[] spatials, int startIndex, int endIndex, Transformation transformation, boolean useTextureAtlas) {
 		/*this.instanceDataBuffer.clear();
-		
+
 		int size = endIndex-startIndex;
 		for (int i = 0; i < size; i++) {
 			Spatial spatial = spatials[i+startIndex];
@@ -249,8 +195,51 @@ public class InstancedMesh extends Mesh {
 
 		glBufferData(GL_ARRAY_BUFFER, instanceDataBuffer, GL_DYNAMIC_DRAW);*/
 	}
-	
+
 	private void renderChunkInstanced(int size, Transformation transformation) {
 		glDrawElementsInstanced(GL_TRIANGLES, vertexCount, GL_UNSIGNED_INT, 0, size);
+	}
+
+	public void setAllVertexAttributesInfo(int start, int iterations, int size, int strideStart, int bytes) {
+		for (int i = 0; i < iterations; i++) {
+			setVertexAttributeInfo(start, size, strideStart);
+			start ++;
+			strideStart += bytes;
+		}
+	}
+
+	public void setVertexAttributeInfo(int start, int size, long strideStart){
+		glVertexAttribPointer(start, size, GL_FLOAT, false, INSTANCE_SIZE_BYTES, strideStart);
+		glVertexAttribDivisor(start, 1);
+	}
+
+	public void setInstancesInfo(int modelMatrixIterations, int lightColorIterations, boolean lightViewMatrix){
+		glBindBuffer(GL_ARRAY_BUFFER, modelViewVBO);
+		int start = 3;
+		int strideStart = 0;
+
+		// Model matrix:
+		setAllVertexAttributesInfo(start, modelMatrixIterations, 4, strideStart, VECTOR4F_SIZE_BYTES);
+		start += modelMatrixIterations;
+		strideStart += VECTOR4F_SIZE_BYTES * modelMatrixIterations;
+
+		// Light Color:
+		setAllVertexAttributesInfo(start,lightColorIterations, 1, strideStart, FLOAT_SIZE_BYTES);
+		start += lightColorIterations;
+		strideStart += FLOAT_SIZE_BYTES * lightColorIterations;
+
+		// Selection:
+		setVertexAttributeInfo(start,1, strideStart);
+		start ++;
+		strideStart += FLOAT_SIZE_BYTES;
+
+		if(lightViewMatrix) {
+			// Light view matrix
+			setAllVertexAttributesInfo(start, 4, 4, strideStart, VECTOR4F_SIZE_BYTES);
+		}
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
 	}
 }
